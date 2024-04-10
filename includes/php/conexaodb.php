0551ab082672a97ca2c65c14475b9d189cb226ea
JPPAlves <joaovictorpenhaalves@gmail.com>
2024-04-10 19:38:34 -0400

    Criando o arquivo de conexão db

---
+++ includes/php/conexaodb.php
@@ -0,0 +1,16 @@
+<?php
+// conexaodb.php
+// Conexão com o banco de dados 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db_PontoEletronico";
+
+// Conectar ao banco de dados
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexão
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+?>
