b228c824cde58f9cbe34a67c4eca36764454c6e4
JPPAlves <joaovictorpenhaalves@gmail.com>
2024-06-12 20:15:33 -0400

    Alterando para salvar no banco Maiuscula

--- includes/php/IncluirPonto/saveincluir.php
+++ includes/php/IncluirPonto/saveincluir.php
@@ -6,7 +6,6 @@ include('/xampp1/htdocs/Projeto/PontoEletronicoPV/includes/php/ValidasSessao/con
 function getAddressFromCoordinates($latitude, $longitude) {
     $apiKey = 'AIzaSyCCQSz3GxJ922XWd1ZhpxJYgb4bKuMmufQ';
     $url = "https://maps.googleapis.com/maps/api/geocode/json?latlng={$latitude},{$longitude}&key={$apiKey}";
-    $url = "https://maps.googleapis.com/maps/api/geocode/json?latlng={$latitude},{$longitude}&key={$apiKey}";
 
     $response = file_get_contents($url);
     $data = json_decode($response, true);
@@ -34,9 +33,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Obter o endereço a partir das coordenadas
         $location = getAddressFromCoordinates($latitude, $longitude);
 
+        // Formatando a data no formato desejado (d-m-Y)
+        $data_formatada = date("Y-m-d", strtotime($data));
+
+        // Converter para maiúsculas
+        $location = strtoupper($location);
+        $hora = strtoupper($hora);
+        $data_formatada = strtoupper($data_formatada);
+        $justificativa = strtoupper($justificativa);
+        $tipo = strtoupper($tipo);
+
         // Agora temos todos os dados necessários, podemos salvar no banco de dados
         $stmt = $conn->prepare("INSERT INTO tbl_registros_ponto(localizacao, hora, data, tipo, justificativa, id_usuario) VALUES (?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssssi", $location, $hora, $data, $tipo, $justificativa, $id_usuario);
+        $stmt->bind_param("sssssi", $location, $hora, $data_formatada, $tipo, $justificativa, $id_usuario);
 
         if ($stmt->execute()) {
             echo "Registro salvo com sucesso!";
